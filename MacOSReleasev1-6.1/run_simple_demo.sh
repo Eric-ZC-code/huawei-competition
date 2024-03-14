ed8d437a582588f0e021b64644b4758cbd1566b4
Yao Chen <yaochenqwer@gmail.com>
2024-03-15 04:41:07 +0800

    java version script

--- MacOSReleasev1-6.1/run_simple_demo.sh
+++ MacOSReleasev1-6.1/run_simple_demo.sh
@@ -1,1 +1,2 @@
-./PreliminaryJudge -m ./maps/map1.txt "java -jar ./output/main.jar"
+./PreliminaryJudge -m ./maps/map1.txt -l WARN "java -jar ./output/main.jar"
+#./PreliminaryJudge -m ./maps/map1.txt -l WARN "python ./sdk/python/main.py"
